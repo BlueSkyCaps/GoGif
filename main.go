78a1fb409df342faac83e28598576b065a7355ff
BlueSkyCaps <linsongxian@outlook.com>
2021-09-25 21:00:23 +0800

    gui call this go project

--- main.go
+++ main.go
@@ -21,11 +21,15 @@ func main() {
 	var w, h, dur, order int64 = 0, 0, 0, 0
 	var inputRoot = ""
 	if len(args) > 1 {
-		w, _ = strconv.ParseInt(args[0], 10, 32)
-		h, _ = strconv.ParseInt(args[1], 10, 32)
-		dur, _ = strconv.ParseInt(args[2], 10, 32)
-		order, _ = strconv.ParseInt(args[3], 10, 32)
-		inputRoot = args[4]
+		// args[0]外部命令行调用时的此进程文件名
+		w, _ = strconv.ParseInt(args[1], 10, 32)
+		h, _ = strconv.ParseInt(args[2], 10, 32)
+		dur, _ = strconv.ParseInt(args[3], 10, 32)
+		order, _ = strconv.ParseInt(args[4], 10, 32)
+		inputRoot = args[5]
+	}
+	for i := 0; i < len(args); i++ {
+		fmt.Println("->->", args[i])
 	}
 	gof.Run(int(w), int(h), float32(dur), int(order), inputRoot)
 	var ce rune
